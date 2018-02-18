18eb12707c24da2377401c6c40d8592c3a561c87
AndreyOOP <andrew_yudaichev@i.ua>
2018-02-18 22:58:28 +0200

    Angular Module3 Assignment (Could be submitted)
    - add directive <found-items> to index.html and to app.js
    - add remove item function to app.js
    - add template listitem.html for the directive

--- NgModule3/js/app.js
+++ NgModule3/js/app.js
@@ -4,34 +4,53 @@
     angular.module('NarrowItDownApp', []).
     controller('NarrowItDownController', NarrowItDownController).
     service('MenuSearchService', MenuSearchService).
-    constant('MenuItemsPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
+    constant('MenuItemsPath', "https://davids-restaurant.herokuapp.com/menu_items.json").
+    directive('foundItems', FoundItemsDirective);
     
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService){
 
-        var contr = this; //todo rename
-
-        contr.sterm = "";
-        contr.found = "";
+        var contr = this; 
 
         contr.narrow = function(){
-            MenuSearchService.getMatchedMenuItems(contr.sterm).then(function(result){ //todo add search term
+            MenuSearchService.getMatchedMenuItems(contr.sterm).then(function(result){
                 
-                // todo temporary;
-                contr.found = "";
-                for(var i=0; i<result.length; i++){
-                    contr.found += result[i].description + "|";
-                }
+                contr.found = result;
+
             }).catch(function(error){
                 console.log(error);
             });
         }
+
+        contr.removeItem = function(ind){
+            MenuSearchService.removeItem(ind);
+        }
     }
+
+    function FoundItemsDirective(){
+        
+        var ddo = {
+            templateUrl: 'listitem.html',
+            scope: {
+                foundItm: '<',  //attribute definition
+                onRemove: '&'   
+            },
+            controller: NarrowListDirectiveController,
+            controllerAs: 'narrower',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
+    function NarrowListDirectiveController(){}
 
     MenuSearchService.$inject = ['$http', 'MenuItemsPath'];
     function MenuSearchService($http, MenuItemsPath){
 
         var service = this;
+
+        var found = [];
 
         service.getMatchedMenuItems = function(searchTerm){
 
@@ -39,7 +58,7 @@
                 url: MenuItemsPath 
             }).then(function(result){
                 
-                var found = [];
+                found = [];
                 var items = result.data.menu_items;
 
                 if(searchTerm){
@@ -55,6 +74,10 @@
             }).catch(function(error){
                 console.log(error);
             });
+        };
+
+        service.removeItem = function(ind){
+            found.splice(ind, 1);
         };
     }
 })
