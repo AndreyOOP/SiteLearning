9715d6a5ac34c4e3a7409c94c8d9bd40c4ff47f0
AndreyOOP <andrew_yudaichev@i.ua>
2018-03-04 21:34:27 +0200

    Add routes js Temporary update link to server in common module Add link to Submit view to index.html

--- NgModule5/src/common/common.module.js
+++ NgModule5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://davids-restaurant.herokuapp.com') //temporary solution
 .config(config);
 
 config.$inject = ['$httpProvider'];
